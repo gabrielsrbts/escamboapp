84b2d782c0773c3c2d6ba2807aa4550c0739d37c
Gabriel Resende <gabrielsrbts@gmail.com>
2017-05-31 23:13:10 +0000

    Creating Edit Member Ads

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,8 +1,21 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit]
 
 	def index
 		#@ads = current_member.ads
 		#@ads = Ad.where(member_id: current_member.id)
     @ads = Ad.to_the(current_member)
 	end
+
+  def edit
+    #
+  end
+
+
+
+  private
+
+  def set_ad
+    @ad = Ad.find(params[:id])
+  end
 end
